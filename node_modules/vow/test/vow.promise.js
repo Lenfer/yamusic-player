9d4b102fa2075007196c22a6a210ee6f69e72a19
Andrey Dergaev <dergaev@yamoney.ru>
2016-09-08 21:58:10 +0300

    Add build enviroment, clean form modules in repo

--- node_modules/vow/test/vow.promise.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    'promise should be immediately fulfilled with argument value' : function(test) {
-        var promise = Vow.promise('val');
-        promise.then(function(val) {
-            test.strictEqual(val, 'val');
-            test.done();
-        });
-    }
-};
